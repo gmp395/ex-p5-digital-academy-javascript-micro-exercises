e505491b82af0116c9e197d7d7f41c6353d80eb7
Gema <gmp395@gmail.com>
2026-06-03 00:11:37 +0200

    ejercicio 1

---
+++ src/ex01/app.js
@@ -0,0 +1,8 @@
+//Escribe un programa que escriba en la pantalla un texto
+//  de tipo <h1> que diga “Hello Javascript”
+
+import { renderTitle } from "./rendering.js";
+
+console.log("app.js cargado");
+
+renderTitle("Hello Javascript");
